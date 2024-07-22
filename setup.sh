1bb0562bbde04a0c240d11d6f6dc859890f6249f
Balbir Singh <134789277+balbirs22@users.noreply.github.com>
2024-07-22 11:19:50 +0530

    added files for deployment

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script will be executed before starting the application
+
+# Install necessary system dependencies
+# Add any system-level installations here, if required
+# Example:
+# apt-get update && apt-get install -y libpq-dev
+
+echo "Setup script executed successfully"
